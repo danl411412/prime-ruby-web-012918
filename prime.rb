336aff6ed834804f0f6211e230abbbbb0be3b1e9
danl411412 <daan.b.lee@gmail.com>
2018-01-24 05:15:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,13 @@ def prime?(num)
   if num <= 1
     return false
   end
-
+  is_it_prime? = true
+  range = (2...num).to_a
+  range.each do |x|
+    if num % x == 0
+      is_it_prime? = false
+      break
+    end
+  end
+  is_it_prime?
 end
